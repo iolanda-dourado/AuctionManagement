56eb0626e632e77aa3139d3182bae3e43c1098ed
Iolanda Dourado <iolanda-dourado@hotmail.com>
2025-01-29 20:42:04 +0000

    Added new features to the Categories service/controller.

--- AuctionManagement.WebAPI/Controllers/CategoriesController.cs
+++ AuctionManagement.WebAPI/Controllers/CategoriesController.cs
@@ -12,43 +12,10 @@ namespace AuctionManagement.WebAPI.Controllers {
     [ApiController]
     public class CategoriesController : Controller {
 
-        private readonly ICategoriesService _categoriesService;
+        private readonly ICategoriesService categoriesService;
 
         public CategoriesController(ICategoriesService categoriesService) {
-            _categoriesService = categoriesService;
-        }
-
-        /// <summary>
-        /// Method to get all categories
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public ActionResult<IEnumerable<CategoryDTO>> GetCategories() {
-            var categoriesDTO = _categoriesService.GetCategories();
-
-            if (categoriesDTO == null || !categoriesDTO.Any()) {
-                return NoContent();
-            }
-
-            return Ok(_categoriesService.GetCategories());
-        }
-
-        /// <summary>
-        /// Method to get a category by id
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("{id}")]
-        public ActionResult<CategoryDTO> GetById(int id) {
-
-            if (_categoriesService == null)
-                return NotFound();
-
-            CategoryDTO category = _categoriesService.GetCategoryById(id);
-
-            if (category == null)
-                return NotFound();
-
-            return Ok(category);
+            this.categoriesService = categoriesService;
         }
 
 
@@ -58,16 +25,84 @@ namespace AuctionManagement.WebAPI.Controllers {
         /// <param name="itemDTOCreate"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult<CategoryDTO> Add(CategoryDTOCreate categoryDTOCreate) {
+        public ActionResult<IEnumerable<CategoryDTO>> Add(CategoryDTOCreate categoryDTOCreate) {
             if (!ModelState.IsValid) {
-                return BadRequest(ModelState); // Retorna erros de validação
+                return BadRequest(ModelState);
             }
 
-            var createdCategory = _categoriesService.AddCategory(categoryDTOCreate);
+            var createdCategory = categoriesService.AddCategory(categoryDTOCreate);
 
             return CreatedAtAction(nameof(Add), new { id = createdCategory.Id }, createdCategory);
         }
 
+
+        /// <summary>
+        /// Method to get all categories
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<CategoryDTO>> GetCategories() {
+            try {
+                var categoriesDTO = categoriesService.GetCategories();
+
+                return Ok(categoriesDTO);
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
+            }
+
+        }
+
+
+        /// <summary>
+        /// Method to get a category by id
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<CategoryDTO>> GetById(int id) {
+            try {
+                CategoryDTO category = categoriesService.GetCategoryById(id);
+                return Ok(category);
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+
+        /// <summary>
+        /// Method to get all categories with items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("withitems")]
+        public ActionResult<IEnumerable<CategoryDTO>> GetCategoriesWithItems() {
+            try {
+                List<CategoryDTO> categoriesDTO = categoriesService.GetCategoriesWithItems();
+
+                return Ok(categoriesDTO);
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+
+
+        /// <summary>
+        /// Method to get all categories without items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("withoutitems")]
+        public ActionResult<IEnumerable<CategoryDTO>> GetCategoriesWithoutItems() {
+            try {
+                List<CategoryDTO> categoriesDTO = categoriesService.GetCategoriesWithoutItems();
+
+                return Ok(categoriesDTO);
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
+            }
+        }
 
 
         /// <summary>
@@ -78,14 +113,15 @@ namespace AuctionManagement.WebAPI.Controllers {
         /// <returns></returns>
         [HttpPut("{id}")]
         public ActionResult Update(int id, Category category) {
+            try {
+                CategoryDTO updatedCateg = categoriesService.UpdateCategory(id, category);
 
-            if (category.Id != id) {
-                return BadRequest();
+                return Ok(updatedCateg);
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
             }
 
-            CategoryDTO updatedCateg = _categoriesService.UpdateCategory(id, category);
-
-            return Ok(updatedCateg);
         }
 
 
@@ -95,17 +131,17 @@ namespace AuctionManagement.WebAPI.Controllers {
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id) {
-            if (_categoriesService == null)
+        public ActionResult Delete(int id) {
+            try {
+                categoriesService.DeleteCategory(id);
                 return NoContent();
-
-            CategoryDTO categoryDTO = _categoriesService.GetCategoryById(id);
-
-            if (categoryDTO == null)
-                return NotFound();
-
-            _categoriesService.DeleteCategory(id);
-            return NoContent();
+            }
+            catch (InvalidOperationException ex) {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (ArgumentException ex) {
+                return NotFound(new { message = ex.Message });
+            }
         }
     }
 }
