d9d2850430c9858c67f6286c56ecab958f0ec988
Iolanda Dourado <iolanda-dourado@hotmail.com>
2025-01-30 10:37:52 +0000

    Updated the database migrations

--- AuctionManagement.WebAPI/Migrations/20250130103659_UpdatedPriceSize.Designer.cs
+++ AuctionManagement.WebAPI/Migrations/20250130103659_UpdatedPriceSize.Designer.cs
@@ -4,6 +4,7 @@ using AuctionManagement.WebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuctionManagement.WebAPI.Migrations
 {
     [DbContext(typeof(AuctionContext))]
-    partial class AuctionContextModelSnapshot : ModelSnapshot
+    [Migration("20250130103659_UpdatedPriceSize")]
+    partial class UpdatedPriceSize
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
